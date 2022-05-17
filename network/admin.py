e4261ebada9ce632d694bdf16f56beaaaa801936
deborahly <97473884+deborahly@users.noreply.github.com>
2022-05-17 16:58:08 -0400

    Change models

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User, Post
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "is_staff", "is_active", "date_joined")
+    list_filter = ("active", "date_joined")
+    search_fields = ("username", "email")
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "poster", "content", "created_on", "edited")
+    list_filter = ("created_on")
+    search_fields = ("username", "email")
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.CharField(max_length=150, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.content}'
