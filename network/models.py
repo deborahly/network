2682c3525f13ea57e2db1bdf71f8bf4fb5ae2358
deborahly <97473884+deborahly@users.noreply.github.com>
2022-05-19 02:50:53 -0400

    Change models

--- network/models.py
+++ network/models.py
@@ -7,9 +7,12 @@ class User(AbstractUser):
 
 class Post(models.Model):
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    content = models.CharField(max_length=150, null=False)
+    content = models.CharField(max_length=150, null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
     
     def __str__(self):
         return f'{self.content}'
+
+    def is_valid_post(self):
+        return self.content != "" and len(self.content) <= 150
