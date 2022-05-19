40306f7bfed2f05cab4445d90bf1109abf901b4e
deborahly <97473884+deborahly@users.noreply.github.com>
2022-05-19 02:51:03 -0400

    Add test

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import User, Post
+
 
 # Create your tests here.
+
+class NewTestCase(TestCase):
+
+    def setUp(self):
+        # Create user
+        user = User.objects.create_user(username="Gigi", email="gigi@example.com", password="12345")
+
+        # Create posts
+        good_post = Post.objects.create(poster=user, content="My name is Bridgette, but you can call me Gigi.")
+        empty_post = Post.objects.create(poster=user, content="")
+        too_long_post = Post.objects.create(poster=user, content="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+    def test_good_post(self):
+        user = User.objects.get(username="Gigi")
+        p = Post.objects.get(content="My name is Bridgette, but you can call me Gigi.")
+        self.assertEqual(p.poster, user)
+
+    def test_empty_post(self):
+        user = User.objects.get(username="Gigi")
+        p = Post.objects.get(content="")
+        self.assertFalse(p.is_valid_post())
+
+    def test_too_long_post(self):
+        user = User.objects.get(username="Gigi")
+        p = Post.objects.get(content="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        self.assertFalse(p.is_valid_post())
+        
